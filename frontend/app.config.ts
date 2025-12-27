6317a5018407e96f00ba69996fd8df6c4dfe1171
mark <mark77234@naver.com>
2025-12-27 15:41:09 +0900

    chore: eas build 설정

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -5,6 +5,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: 'dailyfate',
   slug: 'dailyfate',
   version: '1.0.0',
+  extra: {
+    eas: {
+      projectId: '7eaf22a0-8a86-4d38-96f4-5c8eb183393b',
+    },
+  },
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'light',
@@ -15,17 +20,25 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   assetBundlePatterns: ['**/*'],
   ios: {
+    ...config.ios,
     supportsTablet: true,
     bundleIdentifier: 'com.dailyfate.frontend',
+    infoPlist: {
+      ...config.ios?.infoPlist,
+      ITSAppUsesNonExemptEncryption: false,
+    },
   },
   android: {
+    ...config.android,
     adaptiveIcon: {
+      ...(config.android?.adaptiveIcon ?? {}),
       foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#ffffff',
     },
     package: 'com.dailyfate.frontend',
   },
   web: {
+    ...config.web,
     favicon: './assets/favicon.png',
   },
   scheme: 'frontend',
