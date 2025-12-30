277399b18fd334142ea2d6f9d2f87c7b05ef1539
mark <mark77234@naver.com>
2025-12-30 21:12:44 +0900

    feat: 버전 업데이트

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -9,7 +9,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   // ===== App 기본 정보 =====
   name: '운세한장',
   slug: 'dailyfate',
-  version: '1.0.2',
+  version: '1.0.3',
 
   // ===== 환경 변수 =====
   extra: {
